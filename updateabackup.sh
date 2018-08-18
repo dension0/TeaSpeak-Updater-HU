748eb4eea89b16ad12738841698172f5e5e1d0d5
dension0 <42494964+dension0@users.noreply.github.com>
2018-08-18 16:11:27 +0200

    Update updateabackup.sh

--- updateabackup.sh
+++ updateabackup.sh
@@ -5,11 +5,11 @@
 
 #Usege:
 #download into based of TeaSpeak folder
-#Add command: chmod u+x autoupdateandbackup.sh
-#Run ./autoupdateandbackup.sh
-#-f / --force - forces update (without prompt) example: -f autoupdateandbackup.sh
-#-p path / --path path Sets working path for the script (otherwise executed in the location of the script) example: -p updateshfoldername autoupdateandbackup.sh
-#-s / --start Starts server after successful update via teastart.sh start (you may add name of script with parameters to change this value) example: -s autoupdateandbackup.sh
+#Add command: chmod u+x updateandbackup.sh
+#Run ./updateandbackup.sh
+#-f / --force - forces update (without prompt) example: -f updateandbackup.sh
+#-p path / --path path Sets working path for the script (otherwise executed in the location of the script) example: -p updateshfoldername updateandbackup.sh
+#-s / --start Starts server after successful update via teastart.sh start (you may add name of script with parameters to change this value) example: -s updateandbackup.sh
 
 echo "############### TeaSpeak Updater by dension (based code of Nicer and essemX. THX!) ###############"
 
