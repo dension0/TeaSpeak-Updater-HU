14ff5e6bcb0d8f39b1efc06e54a4f8a2bda953e2
dension0 <42494964+dension0@users.noreply.github.com>
2018-08-19 11:31:06 +0200

    Update updateabackup.sh

--- updateabackup.sh
+++ updateabackup.sh
@@ -5,23 +5,23 @@
 
 #Usege:
 #download into based of TeaSpeak folder
-#Add command: chmod u+x updateandbackup.sh
-#Run ./updateandbackup.sh
-#-f / --force - forces update (without prompt) example: -f updateandbackup.sh
-#-p path / --path path Sets working path for the script (otherwise executed in the location of the script) example: -p updateshfoldername updateandbackup.sh
-#-s / --start Starts server after successful update via teastart.sh start (you may add name of script with parameters to change this value) example: -s updateandbackup.sh
+#Add command: chmod u+x updateabackup.sh
+#Run ./updateabackup.sh
+#-f / --force - forces update (without prompt) example: -f updateabackup.sh
+#-p path / --path path Sets working path for the script (otherwise executed in the location of the script) example: -p updateshfoldername updateabackup.sh
+#-s / --start Starts server after successful update via teastart.sh start (you may add name of script with parameters to change this value) example: -s updateabackup.sh
 
 #Használat:
 #Töltse le a TeaSpeak szerver könyvtárába
 #Parancs: wget https://raw.githubusercontent.com/dension0/TeaSpeak-updatebackupscript/master/updateabackup.sh
 #Jelenlegi felhasználónak addj engedélyt a fájl futtatására
-#Parancs: chmod u+x updateandbackup.sh
+#Parancs: chmod u+x updateabackup.sh
 #Futasd a fájlt
-#Parancs: ./updateandbackup.sh
+#Parancs: ./updateabackup.sh
 #Kapcsolók
-#-f / --force - erőltetett frissítés, frissítés kérdés nélkül (prompt nélkül) példa: -f updateandbackup.sh
-#-p path / --path path A TeaSpeak szerver könyvtárának elérési útja (ha üress a updateandbackup.sh fájl könyvtárába frissít) példa: -p updateshfoldername updateandbackup.sh
-#-s / --start A frissítés végeztével a TeaSpeak szerver újraindítása a teastart.sh start parancssal (az érték megváltoztatásához megadható a parancsfájl neve paraméterrel együtt) példa: -s updateandbackup.sh
+#-f / --force - erőltetett frissítés, frissítés kérdés nélkül (prompt nélkül) példa: -f updateabackup.sh
+#-p path / --path path A TeaSpeak szerver könyvtárának elérési útja (ha üress a updateabackup.sh fájl könyvtárába frissít) példa: -p updateshfoldername updateabackup.sh
+#-s / --start A frissítés végeztével a TeaSpeak szerver újraindítása a teastart.sh start parancssal (az érték megváltoztatásához megadható a parancsfájl neve paraméterrel együtt) példa: -s updateabackup.sh
 
 #color codes from https://raw.githubusercontent.com/Sporesirius/TeaSpeak-Installer/master/teaspeak_install.sh
 function warn() {
